4d8963627487f43f59852a2e0783b5cc054e5a9c
Kauê Correia de Oliveira <163582374+kaueCorreia@users.noreply.github.com>
2025-02-28 21:29:45 -0300

    update hello.java
    Criar uma branch nova e criar um método novo que escreva "Olá Mundo". Salvar e Commitar as alterações.

--- Hello.java
+++ Hello.java
@@ -2,10 +2,15 @@ public class Hello {
 
     public static void main(String[] args) {
         helloWorld();
+        olaMundo();
     }
 
     public static void helloWorld(){
         System.out.println("HelloWorld");
 
+    }
+
+    public static void olaMundo(){
+        System.out.println("ola mundo");
     }
 }
